694dc398c0d35e7e89dfa5da1a5abc78e38776d1
falguni <falguni.sanghani@yahoo.com>
2020-02-10 16:44:21 -0500

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.pagefactory.utils.WebDriverUnpackUtility;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+System.out.println("NativeAppAndroid");
+}
+	@Test
+	public void IOSApps()
+	{
+	System.out.println("IOSApps");	
+	}
+}
